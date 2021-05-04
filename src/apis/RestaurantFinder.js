19140fe4faf5bb1764e9506eac3841e829565fb2
Kevin Johnson <kevinj902302@gmail.com>
2021-05-04 11:46:39 -0400

    Updates api link

--- src/apis/RestaurantFinder.js
+++ src/apis/RestaurantFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://fantastic-food-finder.herokuapp.com/api/v1/restaurants",
+  baseURL: "https://fantastic-food-finder.herokuapp.com:3001/api/v1/restaurants",
 });
